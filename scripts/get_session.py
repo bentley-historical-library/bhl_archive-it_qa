2d09d156694029f4f8d81db5dd5dd777605077ac
maryrosedevega <143856348+maryrosedevega@users.noreply.github.com>
2023-12-08 22:00:18 +0800

    Delete scripts directory

--- scripts/get_session.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def get_session(user_agent):
-	session = requests.Session()
-	session.headers["User-Agent"] = user_agent
-	return session
--- scripts/make_dirs.py
+++
@@ -1,9 +0,0 @@
-import os
-from os.path import join
-
-def make_directories(job_dir):
-	if not os.path.exists(job_dir):
-		print "Making directories"
-		os.makedirs(job_dir)
-		os.makedirs(join(job_dir, "crawled_queued"))
-		os.makedirs(join(job_dir, "statuses"))
